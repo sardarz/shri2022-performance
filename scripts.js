ffb54d3d9fa969cdfd73a28810e7db908134c1e8
sardarz <muhkween@gmail.com>
2022-11-03 23:10:26 +0300

    reposition function

--- scripts.js
+++ scripts.js
@@ -5,45 +5,47 @@
         });
     }
 
+    function selectTab(newId, node) {
+        const newTab = node.querySelector(`.section__tab[data-id=${newId}]`);
+        const newPanel = node.querySelector(`.section__panel[data-id=${newId}]`);
+        const oldTab = node.querySelector('.section__tab_active');
+        const oldPanel = node.querySelector('.section__panel:not(.section__panel_hidden)');
+
+        selected = newId;
+
+        oldTab.classList.remove('section__tab_active');
+        oldTab.setAttribute('aria-selected', 'false');
+        oldTab.removeAttribute('tabindex');
+        newTab.classList.add('section__tab_active');
+        newTab.setAttribute('aria-selected', 'true');
+        newTab.setAttribute('tabindex', '0');
+        newTab.focus({
+            preventScroll: true
+        });
+
+        oldPanel.classList.add('section__panel_hidden');
+        oldPanel.setAttribute('aria-hidden', 'true');
+        newPanel.classList.remove('section__panel_hidden');
+        newPanel.setAttribute('aria-hidden', 'false');
+
+        select.value = newId;
+    }
+
     function makeTabs(node) {
         let selected = node.querySelector('.section__tab_active').dataset.id;
         const tabs = node.querySelectorAll('.section__tab');
         const list = Array.from(tabs).map(node => node.dataset.id);
         const select = node.querySelector('.section__select');
 
-        function selectTab(newId) {
-            const newTab = node.querySelector(`.section__tab[data-id=${newId}]`);
-            const newPanel = node.querySelector(`.section__panel[data-id=${newId}]`);
-            const oldTab = node.querySelector('.section__tab_active');
-            const oldPanel = node.querySelector('.section__panel:not(.section__panel_hidden)');
 
-            selected = newId;
-
-            oldTab.classList.remove('section__tab_active');
-            oldTab.setAttribute('aria-selected', 'false');
-            oldTab.removeAttribute('tabindex');
-            newTab.classList.add('section__tab_active');
-            newTab.setAttribute('aria-selected', 'true');
-            newTab.setAttribute('tabindex', '0');
-            newTab.focus({
-                preventScroll: true
-            });
-
-            oldPanel.classList.add('section__panel_hidden');
-            oldPanel.setAttribute('aria-hidden', 'true');
-            newPanel.classList.remove('section__panel_hidden');
-            newPanel.setAttribute('aria-hidden', 'false');
-
-            select.value = newId;
-        }
 
         select.addEventListener('input', () => {
-            selectTab(select.value);
+            selectTab(select.value, node);
         });
 
         bind(tabs, 'click', event => {
             const newId = event.target.dataset.id;
-            selectTab(newId);
+            selectTab(newId, node);
         });
 
         bind(tabs, 'keydown', event => {
@@ -74,7 +76,7 @@
                 index = list.length - 1;
             }
 
-            selectTab(list[index]);
+            selectTab(list[index], node);
             event.preventDefault();
         });
     }
